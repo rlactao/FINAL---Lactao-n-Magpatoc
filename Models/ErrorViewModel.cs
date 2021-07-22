3aafea399c30907aee7cb81183139119d3057084
abi magpatoc <abimgptc@gmail.com>
2021-07-22 20:29:04 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FINAL___Lactao_n_Magpatoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

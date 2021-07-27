6f852d4eb792029408d637fc3047188e6a634bb3
rlactao <raevictoria.lactao@benilde.edu.ph>
2021-07-28 05:28:49 +0800

    reconnecting to own db

--- Models/MssqlDAL.cs
+++ Models/MssqlDAL.cs
@@ -12,11 +12,11 @@ namespace Layer.DataAccess
 {
     class MssqlDAL
     {
-        private const string ConnectionString = @"Server=DESKTOP-B4U7OC7\SQLEXPRESS;" +
-            "Database=db1;Integrated Security=SSPI";
+        //private const string ConnectionString = @"Server=DESKTOP-B4U7OC7\SQLEXPRESS;" +
+        //    "Database=db1;Integrated Security=SSPI";
 
-        // private const string ConnectionString = @"Server=LAPTOP-4PEMER5Q\MSSQLSERVER2021;" +
-        //    "Database=intepro;UID=MidMPtry;PWD=2021InteproBOLT";
+        private const string ConnectionString = @"Server=LAPTOP-4PEMER5Q\MSSQLSERVER2021;" +
+           "Database=intepro;UID=MidMPtry;PWD=2021InteproBOLT";
 
         private SqlConnection cn = new SqlConnection(ConnectionString);
 
